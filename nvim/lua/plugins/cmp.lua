e147a7f9c8df8bd935f1d3b8f0bede3d033fa746
zxy <zxy267304@gmail.com>
2025-02-12 07:50:08 +0000

    Add nvim-cmp plugins.

---
+++ nvim/lua/plugins/cmp.lua
@@ -0,0 +1,15 @@
+return {
+  "hrsh7th/nvim-cmp",
+  config = function()
+    require('config.cmp')
+  end,
+  dependencies = {
+    "hrsh7th/cmp-nvim-lua",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-calc",
+    "hrsh7th/cmp-nvim-lsp-document-symbol",
+    "hrsh7th/cmp-vsnip",
+  }
+}
