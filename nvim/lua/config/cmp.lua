6d4067cd027980a715755f4868f2e1f7f14724f8
zxy <zxy267304@gmail.com>
2025-02-12 08:00:39 +0000

    Use <tab> to select next code complete.

--- nvim/lua/config/cmp.lua
+++ nvim/lua/config/cmp.lua
@@ -14,6 +14,15 @@ cmp.setup({
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
