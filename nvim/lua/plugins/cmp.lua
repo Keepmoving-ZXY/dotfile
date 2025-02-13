2bf5ac4773cadd85831d2eaeece8e4d0d64469e0
zxy <zxy267304@gmail.com>
2025-02-13 08:32:34 +0800

    Update.

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -4,12 +4,13 @@ return {
     require('config.cmp')
   end,
   dependencies = {
-    "hrsh7th/cmp-nvim-lua",
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
     "hrsh7th/cmp-calc",
     "hrsh7th/cmp-nvim-lsp-document-symbol",
     "hrsh7th/cmp-vsnip",
+    'saadparwaiz1/cmp_luasnip',
   }
 }
